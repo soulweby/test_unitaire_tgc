2082f40321f14a08f10561a7fa089f44c7ad2d9b
Souleyc <souleymane.camara@epitech.eu>
2023-03-20 22:59:37 +0100

    deck test down

--- my_tcg/my_tcg/spec/deck.spec.js
+++ my_tcg/my_tcg/spec/deck.spec.js
@@ -1,8 +1,39 @@
-let year = "2000";
-describe("get year date", () => {
+import Deck from "../src/models/deck";
 
-    year= parseInt(year);
-    it('should  a number', function () {
-        expect(year).toBe(2000)
-    });
+describe("Deck", function () {
+    describe('shuffle', function () {
+        it('Mixed up', function () {
+            const config = { cards: [1, 2, 3, 4] }
+            let deck = new Deck(config)
+
+            expect(deck.shuffle()).toBe(true)
+        })
+
+        it('deck without cards ', function () {
+            const config = { cards: [] }
+            let deck = new Deck(config)
+
+            expect(deck.shuffle()).toBe(false)
+        })
+    })
+
+    describe('draw', function () {
+        it('draw : pioche,retourne et retire la carte', function () {
+            const config = { cards: [1, 2, 3] }
+            const pioche = 1
+            let deck = new Deck(config)
+
+            expect(deck.draw(config)).toEqual(pioche)
+        })
+    })
+
+    describe('getCardsCount', function () {
+        it('getCardsCount : Retourne les cartes du Deck ', function () {
+            const config = { cards: [1, 2, 3] }
+            const pioche = 3
+            let deck = new Deck(config)
+
+            expect(deck.getCardsCount(config)).toEqual(pioche)
+        })
+    })
 })
--- my_tcg/my_tcg/src/models/deck.js
+++ my_tcg/my_tcg/src/models/deck.js
@@ -1,14 +1,26 @@
 
 export default class Deck {
-    constructor (config) {
+    constructor (config){
+        this.cards = config.cards;
     }
 
     shuffle () {
+        if(this.cards.length == 0 || this.cards.some(isNaN)){
+            return false
+        }
+        else{
+            this.cards.sort(function() { return 0.5 - Math.random() });
+            return true;
+        }
     }
 
     draw () {
+        const firstCard = this.cards.shift();
+        return firstCard;
     }
 
     getCardsCount () {
+        const countCard = this.cards.length;
+        return countCard;
     }
 }
